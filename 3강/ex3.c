4f68668ac02cf66c858a6d17b86cfb063ddc3ce3
Myungsang Baek <78290030+myungsangBaek@users.noreply.github.com>
2022-01-03 20:13:45 +0900

    [FEATURE] 4-1

--- 3강/ex3.c
+++ 3강/ex3.c
@@ -3,7 +3,7 @@
 
 int main(void) {
     int a, b, c;
-    scanf("%1d %1d %1d", &a, &b, &c);
+    scanf("%1d%1d%1d", &a, &b, &c);
     printf("%d %d %d\n", a, b, c);
     system("pause");
     return 0;
