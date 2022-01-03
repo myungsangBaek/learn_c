642f402ff70a0d1f97211b3ccb9702575e1d3cf2
Myungsang Baek <78290030+myungsangBaek@users.noreply.github.com>
2022-01-03 20:28:27 +0900

    [FEATURE] 4-4

---
+++ 4강/ex4.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", !a);
+    printf("%d\n", a && b);
+    printf("%d\n", (a > b) && (b > c));
+    system("pause");
+    return 0;
+}
