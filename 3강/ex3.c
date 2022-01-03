cdc1efeaec26cfcaa416126b88eaeac679095c84
Myungsang Baek <78290030+myungsangBaek@users.noreply.github.com>
2022-01-03 20:01:17 +0900

    [FEATURE] ex3-3

---
+++ 3강/ex3.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    scanf("%1d %1d %1d", &a, &b, &c);
+    printf("%d %d %d\n", a, b, c);
+    system("pause");
+    return 0;
+}
