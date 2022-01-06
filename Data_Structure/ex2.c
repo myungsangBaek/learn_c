72019f82c8853a4cf314b44229eae8d68a479528
Myungsang Baek <78290030+myungsangBaek@users.noreply.github.com>
2022-01-07 00:45:53 +0900

    [FEATURE] Data structure ex2 done

--- Data_Structure/ex2.c
+++ Data_Structure/ex2.c
@@ -9,6 +9,20 @@ typedef struct {
 Node *head;
 
 int main(void){
+    head = (Node*) malloc(sizeof(Node));
+    Node *node1 = (Node*) malloc(sizeof(Node));
+    node1->data = 1;
+    Node *node2 = (Node*) malloc(sizeof(Node));
+    node2->data = 2;
+    head->next = node1;
+    node1->next = node2;
+    node2->next = NULL;
+    Node *Cur = head->next;
+    while (cur != NULL){
+        printf("%d ", cur->data);
+            cur = cur->next;
+    }
     systme("pause")
     return 0;
 }
+// 1, 2
