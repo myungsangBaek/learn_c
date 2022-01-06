49dff0f794c8cbc9482055846f6eac060d72e0a8
Myungsang Baek <78290030+myungsangBaek@users.noreply.github.com>
2022-01-07 00:53:20 +0900

    [FEATURE] Generate connect list

--- Data_Structure/ex2.c
+++ Data_Structure/ex2.c
@@ -44,19 +44,16 @@ void showAll(Node *root){
 
 int main(void){
     head = (Node*) malloc(sizeof(Node));
-    Node *node1 = (Node*) malloc(sizeof(Node));
-    node1->data = 1;
-    Node *node2 = (Node*) malloc(sizeof(Node));
-    node2->data = 2;
-    head->next = node1;
-    node1->next = node2;
-    node2->next = NULL;
-    Node *Cur = head->next;
-    while (cur != NULL){
-        printf("%d ", cur->data);
-            cur = cur->next;
-    }
+    head->next = NULL;
+    addFront(head, 2);
+    addFront(head, 1);
+    addFront(head, 7);
+    addFront(head, 9);
+    addFront(head, 8);
+    removeFront(head);
+    showAll(head);
+    freeAll(head);
     systme("pause")
     return 0;
 }
-// 1, 2
+// 9,7,1,2
