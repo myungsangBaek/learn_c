ada31dfe036e00c355156c16fa69b215a28da294
Myungsang Baek <78290030+myungsangBaek@users.noreply.github.com>
2022-01-07 00:51:58 +0900

    [FEATURE] Data structure connect list

--- Data_Structure/ex2.c
+++ Data_Structure/ex2.c
@@ -8,6 +8,40 @@ typedef struct {
 
 Node *head;
 
+// 연결리스트 삽입
+void addFront(Node *root, int data){
+    Node *node = (Node*) malloc(sizeof(Node));
+    node->data = data;
+    node->next = root->next;
+    root->next = node;
+}
+
+void removeFront(Node *root){
+    Node *front = root->next;
+    root->next = front->next;
+    free(front);
+}
+
+//연결 리스트 메모리 해제 함수
+
+void freeAll(Node *root){
+    Node *cur = head->next;
+    while (cur != NULL){
+        Node *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+}
+
+//연결 리스트 전체 출력 함수
+void showAll(Node *root){
+    Node *cur = head->next;
+    while (cur != NULL){
+        printf("%d ", cur->data);
+        cur = cur->next;
+    }
+}
+
 int main(void){
     head = (Node*) malloc(sizeof(Node));
     Node *node1 = (Node*) malloc(sizeof(Node));
